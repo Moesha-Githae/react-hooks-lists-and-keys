a9ca70e822f728ca99253b480502e1e26f86d315
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-04-19 08:30:00 +0300

    complete

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,26 +1,46 @@
 import React from "react";
+import ColorItem from "./ColorItem";
 
 function ColorList() {
   const colors = [
-    "firebrick",
-    "rebeccapurple",
-    "salmon",
-    "darkslategray",
-    "hotpink",
+	 function ColorList() {
+    "hotpink"
+    }];
+
+  //with objects
+  const users = [
+    {id: 1, firstName: "Duane", lastName: "Watson"},
+    {id: 2, firstName: "Dwayne", lastName: "Johnson"},
   ];
+
+  // with non-unique arrays
+  const fib = [0, 1, 1, 3, 5];
+
+  const userHeadings = users.map((user) => {
+    return <h1 key={user.id}>{user.firstName} {user.lastName}</h1>
+  })
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color = {color} />
+  })
+
+  const fibList = fib.map((number, index) => {
+
+    // not the best approach to assigning kays
+    return <div key={index}>{number}</div>
+  })
+
+  console.log(colorElements)
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
+        {userHeadings}
+        {fibList}
       </ol>
     </div>
   );
 }
-
 export default ColorList;
